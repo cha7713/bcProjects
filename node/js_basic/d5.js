d09e69951c0741220117ea39f064e536fb9de23e
unknown <chacha7713>
2019-07-25 10:12:56 +0900

    커밋

---
+++ node/js_basic/d5.js
@@ -0,0 +1,25 @@
+// 모듈화=> 누구나 사용할수 있다
+var mysql      = require('mysql');
+
+exports.selectLogin = ( uid, upw, cb ) => {
+
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'mysql',
+    database : 'node',
+    port : '3300'
+  });
+  connection.connect( (err)=>{  
+    if( err ){    
+    }else{    
+      let sql = `SELECT * FROM users WHERE uid=? AND upw=?; `
+      connection.query( sql, [uid, upw], (err, rows)=>{  
+        console.log( typeof cb)    
+        cb(err, rows)
+        connection.end();      
+      } )
+    }  
+  } );
+
+}
